285cc3e728c90b1a90d067223afb5c2d551d40de
mopoa <mr.pourbaba@gmail.com>
2024-11-13 04:22:23 +0330

    notebook

--- init.py
+++ init.py
@@ -1,15 +1,18 @@
 import numpy as np
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit,QuantumRegister
+from qiskit.primitives import StatevectorSampler
 
-qc= QuantumCircuit(3)
+qreg=QuantumRegister(3,name="q")
+qc = QuantumCircuit(qreg)
+# qc= QuantumCircuit(3)
 
 qc.h(0)
 qc.p(np.pi/2,0)
 qc.cx(0,1)
 qc.cx(0,2)
 measure=qc.measure_all(inplace=False)
-from qiskit.primitives import StatevectorSampler
 sampler= StatevectorSampler()
-job = sampler.run([measure],shots=1000)
-result = job.result()
-print(f" {dir(result)}")
+qc.draw(output="mpl")
+# job = sampler.run([measure],shots=1000)
+# result = job.result()
+# print(f" {dir(result)}")
